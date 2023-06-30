e49c2df6a1364659ba31a92d5a491896d291cfbf
jmendomar <117469151+jmendomar@users.noreply.github.com>
2023-06-30 15:03:01 -0700

    Ease of use update
    Making it easier for linux and windows user to use the program

---
+++ linuxDependInstaller.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python3-pip
+sudo apt-get install python3-tk
+
+python3 dependInstaller.py
+
+python3 newMain.py
---
+++ windowsDependInstaller.ps1
@@ -0,0 +1,7 @@
+winget install "python 3.11" -s msstore
+
+python3 .\dependInstaller.py
+
+python3 .\newMain.py
+
+pause
